fa8b3e21b194f95079f288d7d2bbe92aea154598
Denis Pop <pop.denis1902@gmail.com>
2024-01-01 20:51:07 +0200

    change logo, add images field to posts

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Breadcrumbs from "@mui/material/Breadcrumbs";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import Stack from "@mui/material/Stack";
 import Link from "next/link";
+import Image from "next/image";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -37,9 +38,18 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.variable}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <div className="flex justify-between py-3 px-10 bg-black font-bold text-white">
+          <div className="flex justify-between items-center py-3 px-10 bg-black font-bold text-white">
             <div className="italic font-serif">
-              <Link href="/">All Inclusive Special</Link>
+              <Link href="/" className="flex items-center">
+                <Image
+                  src="/logo3.jpg"
+                  alt="logo"
+                  width={35}
+                  height={35}
+                  className="rounded-full"
+                />
+                <span className="ml-2">All Inclusive Special</span>{" "}
+              </Link>
             </div>
             <Stack spacing={2}>
               <Breadcrumbs
@@ -47,7 +57,7 @@ export default async function RootLayout({
                 aria-label="breadcrumb"
                 className="font-bold text-white"
               >
-                <Link href="/">Principal</Link>
+                <Link href="/">Prima pagina</Link>
                 <Link href="/about">Utilaje</Link>
                 <Link href="/contact">Contact</Link>
               </Breadcrumbs>
