269b8ab7bdf4831c696d53850d1efbaeb067485b
Denis Pop <pop.denis1902@gmail.com>
2024-01-02 01:22:52 +0200

    selling page

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="flex space-x-3">contact</div>
+    </div>
+  );
+}
--- app/utilaje/page.tsx
+++ app/utilaje/page.tsx
@@ -1,6 +1,8 @@
+import Carousel from "@/components/carousel";
 import prisma from "@/lib/prisma";
 import { Paper } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 async function getData() {
   const data = await prisma.post.findMany({
@@ -16,21 +18,46 @@ export default async function Utilaje() {
   const data = await getData();
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <div className="flex space-x-3">utilaje</div>
+    <div className="w-screen h-screen flex flex-col px-8 pt-2">
+      <h1 className="text-5xl font-extrabold text-center text-gray-800 mb-8">
+        Utilaje de vanzare
+      </h1>
+
       {data.map((post) => (
-        <Paper key={post.id} className="h:lg-100vh">
-          <h1>{post.title}</h1>
-          <p>{post.content}</p>
-          {post.images.map((image) => (
-            <Image
-              key={image.id}
-              src={image.url}
-              alt={image.id}
-              width={48}
-              height={48}
-            />
-          ))}
+        <Paper key={post.id} className="w-full h-5/12 py-3 px-4">
+          <div className="flex justify-between h-full">
+            <div className="h-max w-2/4 flex flex-col space-between ">
+              <div className="mt-auto">
+                <h3 className="text-2xl font-bold text-gray-800 mb-4">
+                  {post.title}
+                </h3>
+                <p>Pret: {post.price} &euro;</p>
+                <p>
+                  <Link
+                    href={`/utilaje/${post.id}`}
+                    className="text-blue-500 underline hover:text-blue-700 hover:underline"
+                  >
+                    Mai multe detalii
+                  </Link>
+                </p>
+              </div>
+              <div>
+                <p className="mt-2">
+                  <span className="italic text-gray-500">
+                    Data postarii: {post.createdAt.toLocaleDateString("ro-RO")}
+                  </span>
+                  <span className="mx-2">|</span>
+                  <span className="italic text-gray-500">
+                    Ultima actualizare:{" "}
+                    {post.updatedAt.toLocaleDateString("ro-RO")}
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className="h-3/4 w-1/4">
+              <Carousel imageUrls={post.images.map((image) => image.url)} />
+            </div>
+          </div>
         </Paper>
       ))}
     </div>
