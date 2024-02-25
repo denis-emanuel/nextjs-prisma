f8681798c890dd8d6526614be92a50f62f5cfe55
Denis Pop <pop.denis1902@gmail.com>
2024-02-25 21:35:57 +0200

    change separators between navbar components

--- components/navbar/desktop-nav.tsx
+++ components/navbar/desktop-nav.tsx
@@ -1,14 +1,16 @@
 import Breadcrumbs from "@mui/material/Breadcrumbs";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import NavLink from "./nav-link";
+
 import { NavbarItems } from "types/constants/navbar";
+import NavLink from "./nav-link";
+
+import CircleIcon from "@mui/icons-material/Circle";
 
 export default function DesktopNav() {
   return (
     <>
       <ul className="hidden h-full gap-12 lg:flex">
         <Breadcrumbs
-          separator={<NavigateNextIcon fontSize="small" />}
+          separator={<CircleIcon fontSize="small" className="square h-2" />}
           aria-label="breadcrumb"
           className="font-bold text-white"
         >
