e97e7c021b5c73024e1e6fef09eb9ac5fb37954a
Denis Pop <pop.denis1902@gmail.com>
2024-02-25 22:08:11 +0200

    add emojis to contact page

--- components/contact/info.tsx
+++ components/contact/info.tsx
@@ -4,10 +4,10 @@ export default function ContactInfo() {
       <h2 className="text-2xl mb-2 lg:text-3xl lg:mb-5">
         Informatii de contact
       </h2>
-      <p>Persoana de contact: Dunca Liviu</p>
+      <p>👤 Persoana de contact: Dunca Liviu</p>
 
       <p>
-        Telefon:{" "}
+        📞 Telefon:{" "}
         <a
           href="tel:0758843994"
           className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
@@ -16,7 +16,7 @@ export default function ContactInfo() {
         </a>
       </p>
       <p>
-        E-Mail:{" "}
+        📧 E-Mail:{" "}
         <a
           href="mailto:pop.denis1902@gmail.com"
           className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
@@ -25,7 +25,7 @@ export default function ContactInfo() {
         </a>
       </p>
       <p>
-        Locatie:{" "}
+        📍 Locatie:{" "}
         <a
           href="https://www.google.com/maps/place/All+Inclusive+Special/@47.1347657,24.4757935,16.82z/data=!4m6!3m5!1s0x4749f7e76c0499b7:0xb13708bdb04d5104!8m2!3d47.1356283!4d24.4758462!16s%2Fg%2F11llbgpgw5?entry=ttu"
           className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
