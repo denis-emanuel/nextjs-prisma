12437087cab02eeb339799a6f71d0b5030ec1df7
Denis Pop <pop.denis1902@gmail.com>
2024-05-20 21:05:46 +0300

    add machinery design page

--- app/protected/adauga-utilaj/page.tsx
+++ app/protected/adauga-utilaj/page.tsx
@@ -1,40 +1,81 @@
 "use client";
 
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import { useFormState, useFormStatus } from "react-dom";
+import CircularProgress from "@mui/material/CircularProgress";
 
 import uploadImage from "@/app/actions/upload-image";
+import { useState } from "react";
 
 const initialState = {
   file: "",
 };
 
 export default function CreateUtilaj() {
-  // const session = getServerSession();
-  // if (!session || !session.user) {
-  //   redirect("/");
-  // }
   const { pending } = useFormStatus();
   const [formState, formAction] = useFormState(uploadImage, initialState);
 
   return (
-    <div className="container mx-auto pt-5">
-      {/* <span>sal {session.user?.email}</span> */}
+    <div className="container mx-auto mt-2 md:mt-5 p-3 md:p-5">
+      <h2 className="text-2xl mb-2 lg:text-3xl lg:mb-5">Anunt nou</h2>
 
-      <form action={formAction}>
-        <input
-          type="file"
-          name="file"
-          onChange={(e) => {
-            if (e.target.files) {
-              console.log(e.target.files[0]);
-              console.log(e.target.files[0].type.startsWith("image/"));
-            }
-          }}
-        />
-        <hr />
-        <button type="submit">upload</button>
+      <form action={formAction} className="flex flex-col space-y-4">
+        <div className="flex flex-col">
+          <label htmlFor="name" className="text-sm font-medium">
+            Titlu
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="p-1 rounded-lg border border-gray-400"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="name" className="text-sm font-medium">
+            Pret (€)
+          </label>
+          <input
+            type="number"
+            name="price"
+            id="price"
+            className="p-1 rounded-lg border border-gray-400"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="file"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Imagini
+          </label>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            multiple
+            className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="name" className="text-sm font-medium">
+            Descriere
+          </label>
+          <textarea
+            name="description"
+            id="description"
+            className="p-1 rounded-lg border border-gray-400"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="text-primary bg-gray-600 p-2 rounded-md md:w-1/5"
+        >
+          {pending ? <CircularProgress /> : "Adauga anunt"}
+        </button>
       </form>
     </div>
   );
