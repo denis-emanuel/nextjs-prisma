9d60117ee7989fdce6a45bc557e9008ec3fda0a0
Denis Pop <pop.denis1902@gmail.com>
2024-05-26 00:46:13 +0300

    possible fix for private key not working

--- app/actions/upload-image.ts
+++ app/actions/upload-image.ts
@@ -7,7 +7,9 @@ async function uploadImage(imageName: string, image: string | Blob) {
     projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
     credentials: {
       client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY,
+      private_key: process.env
+        .GOOGLE_CLOUD_PRIVATE_KEY!.split(String.raw`\n`)
+        .join("\n"),
     },
   });
   const bucket = storage.bucket(process.env.GOOGLE_CLOUD_BUCKET_NAME!);
